bb468398a8740e3ec35c752a3a5506f9eecbff92
Ramy Sabry <ramysabry1996@gmail.com>
2022-05-19 00:41:30 +0200

    project infrastructure

---
+++ Koinz Task/Scenes/Movies List/Presentation/View/MoviesListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MoviesListViewController.swift
+//  Koinz Task
+//
+//  Created by Ramy Sabry on 19/05/2022.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
